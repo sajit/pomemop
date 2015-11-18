a328a326109c9d97dd8a6c804caeb4910482a20c
Sajit Mathew Kunnumkal <sajit.kunnumkal@accenturefederal.com>
2015-11-18 18:32:43 -0500

    All but watch working.

--- src/main/webapp/static/Gruntfile.js
+++ src/main/webapp/static/Gruntfile.js
@@ -5,6 +5,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        mavenPrepare: {
+            options: {
+                resources: ['sass/**','Gruntfile.js','package.json']
+            }
+        },
         sass: {
             dist: {
                 files: {
@@ -12,12 +17,20 @@ module.exports = function(grunt) {
                 }
             }
         },
+        gruntMavenProperties: grunt.file.readJSON('grunt-maven.json'),
+
         watch: {
-            css: {
-                files: '**/*.scss',
-                tasks: ['sass']
+            maven: {
+                files: ['<%= gruntMavenProperties.filesToWatch %>'],
+                tasks: 'default'
             }
         },
+        //watch: {
+        //    css: {
+        //        files: '**/*.scss',
+        //        tasks: ['sass']
+        //    }
+        //},
         kss: {
             options: {
                 includeType: 'css',
@@ -32,8 +45,10 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-kss');
-    //grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('dosass',['sass']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-maven');
+    grunt.registerTask('css',['sass']);
     grunt.registerTask('styleguide', ['kss']);
-    grunt.registerTask('default',['sass','kss']);
+    grunt.registerTask('default',['mavenPrepare','sass','kss']);
 };
